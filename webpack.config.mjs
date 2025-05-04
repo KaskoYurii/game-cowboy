b08c65cb89c1cb2c46b34c64f8f8609d5dfa1638
KaskoYurii <hellokasko@gmail.com>
2025-05-04 19:59:42 +0300

    GH pages CI config

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -17,6 +17,7 @@ export default (_env, argv) => {
       path: path.resolve(process.cwd(), "dist"),
       filename: "bundle.js",
       clean: true,
+      publicPath: "/game-cowboy/",
     },
 
     // Config for your testing server
